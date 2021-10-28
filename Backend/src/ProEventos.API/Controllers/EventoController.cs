3e4a0b0d11024ed7f593d2eb39305d93f867c667
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2021-10-28 11:42:44 -0300

    Primeiro Commit

---
+++ Backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController(ILogger<EventoController> logger)
+        {
+           
+        }
+
+        [HttpGet]
+        public List<Evento> Get()
+        {
+            return new List<Evento>()
+            {
+                new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "Encontro Turma .NET",
+                    Local = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString()
+                },
+                new Evento()
+                {
+                    EventoId = 2,
+                    Tema = "Segundo Encontro Turma .NET",
+                    Local = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString()
+                }
+            };
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Método Post";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Método Delete Com o id:{id}";
+        }
+    }
+}
